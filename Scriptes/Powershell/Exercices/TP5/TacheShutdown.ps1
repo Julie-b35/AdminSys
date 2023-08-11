5bc7ebd4159b55f196f5863e69cab54f917149d6
Julie-b35 <71323633+Julie-b35@users.noreply.github.com>
2023-08-11 14:17:29 +0200

    l

--- Scriptes/Powershell/Exercices/TP5/TacheShutdown.ps1
+++ Scriptes/Powershell/Exercices/TP5/TacheShutdown.ps1
@@ -1,1 +1,14 @@
 
+$user = 'AD01\Administrator'
+$mdp = Read-Host "Mot de passe : $user "  -AsSecureString 
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $mdp
+$session = New-PSSession -ComputerName 172.21.0.3 -Credential $credential
+
+#Get-Module -PSSession $Session -ListAvailable
+#Import-Module -PSSession $Session –name ActiveDirectory –Prefix DC
+
+Enter-PSSession $session 
+
+hostname.exe
+
+Exit-PSSession
